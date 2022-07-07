6add2e73acb9a83e617aec95ec9cf1861796e0f5
SoulsArchitecht <serje_fox@mail.ru>
2022-07-07 14:22:39 +0800

    changedDbMigrations

--- src/main/java/sshibko/myblog/api/response/PostListResponse.java
+++ src/main/java/sshibko/myblog/api/response/PostListResponse.java
@@ -1,5 +1,6 @@
 package sshibko.myblog.api.response;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -11,5 +12,6 @@ import java.util.List;
 @AllArgsConstructor
 public class PostListResponse {
     private int count;
+    @JsonProperty("posts")
     private List<PostResponse> postResponseList;
 }
